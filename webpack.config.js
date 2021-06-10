f29d8d655f9abdddbbb88390a3cd8d6dfd3cc6ae
TzuHanLiang <crocodilestear@gmail.com>
2021-06-10 11:39:00 +0800

    feat: basic setup

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const webpack = require("webpack");
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const frontend = {
+    mode: "development",
+    entry: path.resolve(__dirname, "src/main.js"),
+    output: {
+      path: path.resolve(__dirname, "build"),
+      filename: "javascript/bundle.js",
+      chunkFilename: "[id].js",
+    },
+    devServer: {
+      watchOptions: {
+        poll: true,
+      },
+      contentBase: path.join(__dirname, "build/"),
+      compress: true,
+      port: 9000,
+    },
+  
+    module: {
+      rules: [
+        {
+          test: /\.(sa|sc|c)ss$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            "css-loader",
+            "postcss-loader",
+            "sass-loader",
+          ],
+        },
+        {
+          test: /\.(png|jpe?g|gif)$/i,
+          use: [
+            {
+              loader: "file-loader",
+              options: {
+                name: "image/[name].[ext]", // 修改生成路徑
+                esModule: false,
+                publicPath: "../",
+              },
+            },
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new webpack.ProvidePlugin({
+        Buffer: ['buffer', 'Buffer'],
+      }),
+      new webpack.NoEmitOnErrorsPlugin(),
+      new MiniCssExtractPlugin({
+        filename: "css/[name].css",
+        chunkFilename: "css/[id].css",
+      }),
+      new webpack.HotModuleReplacementPlugin(),
+    ],
+  };
+
+  module.exports = [frontend];
